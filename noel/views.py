7a77e9ec7b398f1a5c8583cf47266f1bbbe3301b
Noel <noeleon930@gmail.com>
2015-04-28 20:50:00 +0800

    almost tutorial done

--- noel/views.py
+++ noel/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def hello_world(request):
     return render(request, 'hello_world.html', {'current_time': datetime.now()})
+
+
+def home(request):
+	# get all the posts
+	post_list = Post.objects.all()
+	return render(request, 'home.html', {'post_list': post_list})
